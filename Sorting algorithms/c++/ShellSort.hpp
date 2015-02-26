cfc157c7a066ed2bce66bfc1fee40e5c72bfe0cb
Chafic Najjar <chafic.najjar@gmail.com>
2015-02-26 23:18:36 +0200

    Added worst case performance information for gap = N/2^K

--- Sorting algorithms/c++/ShellSort.hpp
+++ Sorting algorithms/c++/ShellSort.hpp
@@ -5,6 +5,7 @@
  * Worst case performance is O(n^2)
  * Best case performance is O(nlog^2(n))
  * Average case performance depends on gap sequence
+ * For gap = N/2^k, worst-case performance is O(n^2)
  * Worst case space complexity is O(n) total, O(1) auxiliary
  * 
  * */
